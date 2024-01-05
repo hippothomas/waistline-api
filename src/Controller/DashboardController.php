d37606f28db7051368008b7b874e839653dff748
Hippolyte Thomas <contact@hippolyte-thomas.fr>
2024-01-05 20:01:46 +1300

    feat: add style & change access control

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends AbstractController
 {
-    #[Route('/dashboard', name: 'dashboard')]
+    #[Route('/', name: 'dashboard')]
     public function index(): Response
 	{
         $username = $this->getUser()->getUsername();

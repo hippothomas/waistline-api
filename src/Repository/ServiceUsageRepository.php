60d175560f3d9c39db84e752e2c69a8d9c712b24
Hippolyte Thomas <contact@hippolyte-thomas.fr>
2024-01-06 14:01:33 +1300

    feat: handle daily limit usage for API

--- src/Repository/ServiceUsageRepository.php
+++ src/Repository/ServiceUsageRepository.php
@@ -3,7 +3,10 @@
 namespace App\Repository;
 
 use App\Entity\ServiceUsage;
+use App\Entity\User;
+use DateTime;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Exception\ORMException;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -20,4 +23,42 @@ class ServiceUsageRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, ServiceUsage::class);
     }
+
+	/**
+	 * Get the service usage of a certain user based on today's date
+	 * @param User $user User object we want to search
+	 * @return ServiceUsage If the service usage for today exist we return it else we return a new service usage object.
+	 */
+	public function getDailyUsage(User $user): ServiceUsage
+	{
+		$date = new DateTime();
+		$query = $this->createQueryBuilder('u')
+					->andWhere('u.account = :user')
+					->andWhere('u.date = :date')
+					->setParameter('user', $user)
+					->setParameter('date', $date->format('Y-m-d'))
+					->setMaxResults(1)
+					->getQuery();
+		try {
+			return $query->getSingleResult();
+		} catch (ORMException) {
+			$serviceUsage = new ServiceUsage();
+			$serviceUsage->setAccount($user);
+			$serviceUsage->setUsage(0);
+			$serviceUsage->setDate($date);
+			return $serviceUsage;
+		}
+	}
+
+	/**
+	 * Insert or Update the incremented daily usage
+	 * @param ServiceUsage $serviceUsage
+	 * @return void
+	 */
+	public function upsertDailyUsage(ServiceUsage $serviceUsage): void
+	{
+		$serviceUsage->setUsage($serviceUsage->getUsage()+1);
+		$this->getEntityManager()->persist($serviceUsage);
+		$this->getEntityManager()->flush();
+	}
 }

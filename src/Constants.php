100fa5525adb1c4294f52480f9dabf50fc5ffde1
Hippolyte Thomas <contact@hippolyte-thomas.fr>
2024-01-22 14:04:32 +1300

    feat: add constants & app version

---
+++ src/Constants.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+class Constants
+{
+    /**
+     * The current release version
+	 * Follows the Semantic Versioning strategy: https://semver.org/
+     */
+    public const string VERSION = '1.0.0';
+    /**
+     * The current release: major * 10000 + minor * 100 + patch
+     */
+    public const int VERSION_ID = 10000;
+    /**
+     * Documentation URL
+     */
+    public const string DOCS_URL = 'https://docs.waistline-api.hippolyte-thomas.fr';
+}

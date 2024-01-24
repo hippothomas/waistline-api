297b50fcd182e983741bf745650ec665b9ea819f
Hippolyte Thomas <contact@hippolyte-thomas.fr>
2024-01-24 15:47:56 +1300

    feat: add application monitoring metrics w/ sentry

--- src/Constants.php
+++ src/Constants.php
@@ -8,11 +8,11 @@ class Constants
      * The current release version
 	 * Follows the Semantic Versioning strategy: https://semver.org/
      */
-    public const string VERSION = '1.0.0';
+    public const string VERSION = '1.1.0';
     /**
      * The current release: major * 10000 + minor * 100 + patch
      */
-    public const int VERSION_ID = 10000;
+    public const int VERSION_ID = 10100;
     /**
      * Documentation URL
      */

2b51a0e30aac8a485b8818abe09cef14ad47b804
Hippolyte Thomas <contact@hippolyte-thomas.fr>
2025-01-13 11:06:09 +0100

    fix: update dependencies with vulnerabilities (symfony)

--- src/Constants.php
+++ src/Constants.php
@@ -8,11 +8,11 @@ class Constants
      * The current release version
 	 * Follows the Semantic Versioning strategy: https://semver.org/
      */
-    public const string VERSION = '1.1.1';
+    public const string VERSION = '1.1.2';
     /**
      * The current release: major * 10000 + minor * 100 + patch
      */
-    public const int VERSION_ID = 10101;
+    public const int VERSION_ID = 10102;
     /**
      * Documentation URL
      */
